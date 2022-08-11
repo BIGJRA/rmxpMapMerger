d20ad5fbb1897eef6b1eb7e9b41c7f188a2eaa02
BIGJRA <47573834+BIGJRA@users.noreply.github.com>
2022-08-11 17:25:46 -0400

    Support for 202 events

--- src/map_merger.rb
+++ src/map_merger.rb
@@ -57,7 +57,7 @@ def get_merged_map(map_yaml_hash, destination_num)
           page.list.each do |command|
 
             if command.code == 201 then # Transfer Player. 
-              # 6 Params: ??? (ALWAYS IS 0 IN REBORN), Destination Map ID, Dest X, Dest Y, Retain, Fade
+              # 6 Params: Declaration Type (?) (ALWAYS IS 0 IN REBORN), Destination Map ID, Dest X, Dest Y, Direction, Fade
               if offset_hash.keys.include?(command.parameters[1]) then
                 command.parameters[2] += offset_hash[command.parameters[1]][0]
                 command.parameters[3] += offset_hash[command.parameters[1]][1]
@@ -65,9 +65,15 @@ def get_merged_map(map_yaml_hash, destination_num)
               end
             end
 
-
-
-
+            if command.code == 202 then # Set Event Location. 
+              # 5 Params: Event ID, Declaration Type, Dest X, Dest Y, Direction, Fade
+              command.parameters[0] += event_num_offset_hash[map_num]
+              if (command.parameters[1] == 0) then
+                command.parameters[2] += offset_hash[map_num][0]
+                command.parameters[3] += offset_hash[map_num][1]
+              else puts ("Variable Set Event Location, in event with name " + event.name + " found on map " + map_num.to_s + ". Skipping...")
+              end
+            end
 
           end
         end
